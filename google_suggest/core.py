c9fd1600d455d057f80b04824bc32f0a6dc4a3ab
kishibashi <kazuya.ishibashi.wn@gmail.com>
2019-12-15 22:59:31 +0900

    modularize.

--- google_suggest/core.py
+++ google_suggest/core.py
@@ -4,13 +4,26 @@ import requests
 from xml.etree import ElementTree
 
 
-def fetch_suggestion(keyword) -> List[str]:
-    keyword: str = re.sub('[ 　]+', '+', keyword)
+def fetch_suggestion(keyword: str) -> List[str]:
+    keyword = _shape_request_data(keyword)
+    response = _request(keyword)
+    return _parse(response)
+
+
+def _shape_request_data(keyword: str) -> str:
+    return re.sub('[ 　]+', '+', keyword)
+
+
+def _request(keyword: str):
     response = requests.get(f'https://www.google.com/complete/search?hl=ja&output=toolbar&q={keyword}')
     response.raise_for_status()
+    return response
 
-    suggests_xml: str = response.text
-    root = ElementTree.fromstring(suggests_xml)
+
+def _parse(response) -> List[str]:
+    xml: str = response.text
+    root = ElementTree.fromstring(xml)
+    completeSuggestionTags = root.findall('CompleteSuggestion')
     return [ suggestion.find('suggestion').get('data') for suggestion in root.findall('CompleteSuggestion') ]
 
 
